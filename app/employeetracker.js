46cf676a9f9556af1d0daf37d8321e0f5e3c0323
Vik Boyechko <vik.boyechko@gmail.com>
2024-01-27 20:54:53 -0600

    add department working

--- app/employeetracker.js
+++ app/employeetracker.js
@@ -24,22 +24,193 @@ function employeeTracker(db) {
   ];
 
   inquirer.prompt(menuPrompt).then((response) => {
+    // View All Departments
     if (response.menu === 'View All Departments') {
       console.clear();
-      console.log('\n**Viewing All Departments**\n');
-      db.query('SELECT * FROM department', function (err, results) {
-        if (err) {
-          console.error('Error executing the query:', err);
-          returnToMenu(db);
-        } else {
-          console.table(results);
-          returnToMenu(db);
+      console.log('\n** Viewing All Departments **\n');
+      db.query(
+        `
+      
+      SELECT * FROM department
+      
+      
+      `,
+        function (err, results) {
+          if (err) {
+            console.error('Error executing the query:', err);
+            returnToMenu(db);
+          } else {
+            console.table(results);
+            returnToMenu(db);
+          }
         }
-        // Continue using the db connection for other queries or operations
-      });
-    } else {
-      //   db.end();
-      return;
+      );
+
+      // View All Roles
+    } else if (response.menu === 'View All Roles') {
+      console.clear();
+      console.log('\n** Viewing All Roles **\n');
+      db.query(
+        `
+        
+        SELECT * FROM role
+        
+        `,
+        function (err, results) {
+          if (err) {
+            console.error('Error executing the query:', err);
+            returnToMenu(db);
+          } else {
+            console.table(results);
+            returnToMenu(db);
+          }
+        }
+      );
+
+      // View All Employees
+    } else if (response.menu === 'View All Employees') {
+      console.clear();
+      console.log('\n** Viewing All Employees **\n');
+      db.query(
+        `
+          
+          SELECT * FROM employee
+          
+          `,
+        function (err, results) {
+          if (err) {
+            console.error('Error executing the query:', err);
+            returnToMenu(db);
+          } else {
+            console.table(results);
+            returnToMenu(db);
+          }
+        }
+      );
+
+      // Add Department
+    } else if (response.menu === 'Add Department') {
+      console.clear();
+      console.log('\n** Add Department **\n');
+      inquirer
+        .prompt([
+          {
+            type: 'input',
+            message: 'New Department Name: ',
+            name: 'name',
+          },
+        ])
+        .then((response) => {
+          const departmentName = response.name;
+
+          db.query(
+            `
+                    
+                    INSERT INTO department (name)
+                    VALUES
+                        (?)
+                    
+                    `,
+            [departmentName],
+            function (err, results) {
+              if (err) {
+                console.error('Error executing the query:', err);
+                returnToMenu(db);
+              } else {
+                console.table(results);
+                returnToMenu(db);
+              }
+            }
+          );
+        });
+
+      // Add Role
+    } else if (response.menu === 'Add Role') {
+      console.clear();
+      console.log('\n** Add Role **\n');
+      inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'New Role Title: ',
+          name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Salary: '
+            name: 'salary',
+        }
+        {
+            type: 'input',
+            message: 'Department: ',
+            name: 'department',
+        }
+
+      ])
+      .then((response) => {
+        const roleTitle = response.name;
+
+        db.query(
+          `
+                  
+                  INSERT INTO role (title)
+                  VALUES
+                      (?)
+                  
+                  `,
+          [roleTitle],
+        function (err, results) {
+          if (err) {
+            console.error('Error executing the query:', err);
+            returnToMenu(db);
+          } else {
+            console.table(results);
+            returnToMenu(db);
+          }
+        }
+      );
+
+      // Add Employee
+    } else if (response.menu === 'Add Employee') {
+      console.clear();
+      console.log('\n** Add Employee **\n');
+      db.query(
+        `
+            
+            SELECT * FROM employee
+            
+            `,
+        function (err, results) {
+          if (err) {
+            console.error('Error executing the query:', err);
+            returnToMenu(db);
+          } else {
+            console.table(results);
+            returnToMenu(db);
+          }
+        }
+      );
+
+      // Update Employee Role
+    } else if (response.menu === 'Update Employee Role') {
+      console.clear();
+      console.log('\n** Update Employee Role **\n');
+      db.query(
+        `
+            
+            SELECT * FROM employee
+            
+            `,
+        function (err, results) {
+          if (err) {
+            console.error('Error executing the query:', err);
+            returnToMenu(db);
+          } else {
+            console.table(results);
+            returnToMenu(db);
+          }
+        }
+      );
     }
   });
 }
